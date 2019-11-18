fb32e926666f3657ef722b10db544f2457c09d12
waready <elbarto119@hotmail.com>
2019-11-17 21:53:39 -0500

    deploy2

--- src/App.vue
+++ src/App.vue
@@ -43,12 +43,12 @@
           <div id="data-container">
           <div  id='canvas'></div>
           <!-- Button trigger modal -->
-          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+          <!-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
             ver tabla de datos
-          </button>
+          </button> -->
 
           <!-- Modal -->
-          <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+          <!-- <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
             <div class="modal-dialog" role="document">
               <div class="modal-content">
                 <div class="modal-header">
@@ -67,7 +67,7 @@
                 </div>
               </div>
             </div>
-          </div>
+          </div> -->
                       <!-- <h3>Sample Predictions</h3>
             <table id='samples'>
             </table> -->
